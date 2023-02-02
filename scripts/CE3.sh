ff1b278fadfe8809cdbc899a93ef1107e671e9a6
Sara-DaCanal <s.saradacanal@gmail.com>
2023-02-02 14:16:18 +0100

    modifiche

--- scripts/CE3.sh
+++ scripts/CE3.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sysctl -w net.ipv4.ip_forward=1
-ip link set enp0s3 up
-ip link set enp0s8 up
-ip addr add 10.3.11.2/30 dev enp0s3
-ip addr add 10.23.1.1/24 dev enp0s8
+ip link set eth0 up
+ip link set eth1 up
+ip addr add 10.3.11.2/30 dev eth0
+ip addr add 10.23.1.1/24 dev eth1
+
+ip route add default via 10.3.11.1
