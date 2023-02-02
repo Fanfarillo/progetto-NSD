568e442a67d3d02b3e34fbb795fb835f943ebb09
Sara-DaCanal <s.saradacanal@gmail.com>
2023-02-02 14:15:53 +0100

    tutti i ce

---
+++ scripts/CE2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sysctl -w net.ipv4.ip_forward=1
+ip link set eth0 up
+ip link set eth1 up
+ip addr add 10.2.13.2/30 dev eth0
+ip addr add 10.123.0.1/16 dev eth1
+
+ip route add default via 10.2.13.1
