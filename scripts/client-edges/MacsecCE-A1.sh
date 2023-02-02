a93ae0aea8a2009172913c824631904238e0ba0c
Fanfarillo <matteo.fanfarillo99@gmail.com>
2023-02-02 22:05:16 +0100

    Fixed Macsec Key Agreement

---
+++ scripts/client-edges/MacsecCE-A1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export MKA_CAK=00112233445566778899aabbccddeeff
+export MKA_CKN=00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff
+
+nmcli connection add type macsec \
+con-name macsec-123 \
+ifname macsec0 \
+connection.autoconnect no \
+macsec.parent enp0s8 \
+macsec.mode psk \
+macsec.mka-cak $MKA_CAK \
+macsec.mka-cak-flags 0 \
+macsec.mka-ckn $MKA_CKN \
+ipv4.addresses 10.23.0.1/24
+
+nmcli connection up macsec-123
