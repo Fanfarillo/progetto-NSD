911d6dc76545e1bfe527c8301c604aa9e1cdea87
Fanfarillo <matteo.fanfarillo99@gmail.com>
2023-02-03 12:32:26 +0100

    Fixed FirewallCE-A1.sh

--- scripts/client-edges/FirewallCE-A1.sh
+++ scripts/client-edges/FirewallCE-A1.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 export LAN=enp0s8
 export AS=enp0s3
-
 
 #abilitare il forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -17,7 +17,7 @@ iptables -P OUTPUT ACCEPT
 #permette tutto il traffico dalla lan in uscita
 iptables -A FORWARD -i $LAN -o $AS -j ACCEPT
 #con dynamic source address translation
-iptables -t nat -A POSTROUTING -i $LAN -o $AS -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $AS -j MASQUERADE
 
 #deny traffico verso il router tranne ssh e icmp dalla lan
 iptables -A INPUT -i $LAN -p tcp --dport 22 -j ACCEPT
@@ -28,5 +28,5 @@ iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
 
 #accetta entrata verso la lan se http con dnat
 iptables -A FORWARD -i $AS -o $LAN -p tcp --dport 80 -j ACCEPT
-iptables -t nat -A PREROUTING -i $AS -o $LAN -j DNAT --to-destination 10.23.0.10 
-iptables -t nat -A PREROUTING -i $AS -o $LAN -j DNAT --to-destination 10.23.0.20
+iptables -t nat -A PREROUTING -i $AS -j DNAT --to-destination 10.23.0.10 
+iptables -t nat -A PREROUTING -i $AS -j DNAT --to-destination 10.23.0.20
