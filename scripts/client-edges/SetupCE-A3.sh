03d9b6d221fd29aeaad0c38bc9123f48e190d3e3
Fanfarillo <matteo.fanfarillo99@gmail.com>
2023-02-04 19:16:09 +0100

    Ho seguito alcune indicazioni sulla configurazione della rete. Resto però in attesa di un riscontro anche da parte del professore.

--- scripts/client-edges/SetupCE-A3.sh
+++ scripts/client-edges/SetupCE-A3.sh
@@ -3,7 +3,7 @@
 sysctl -w net.ipv4.ip_forward=1
 ip link set eth0 up
 ip link set eth1 up
-ip addr add 10.3.11.2/30 dev eth0
+ip addr add 100.3.11.2/30 dev eth0
 ip addr add 10.23.1.1/24 dev eth1
 
-ip route add default via 10.3.11.1
+ip route add default via 100.3.11.1
