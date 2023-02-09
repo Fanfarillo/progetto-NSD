a46ee717722a34f1ffd50cfc6012688b35235f59
MegAtonBoom <baba.adrian13@gmail.com>
2023-02-09 17:34:18 +0100

    Added some Setup files

---
+++ scripts/hosts/lan-B/SetupOpenvpn-server.sh
@@ -0,0 +1,7 @@
+ip addr add 2.0.0.2/30 dev eth0
+ip addr add 192.168.17.1/24 dev eth1
+
+ip route add default via 2.0.0.1
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
