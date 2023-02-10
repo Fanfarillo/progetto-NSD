ac95a2d27d6b337d1ff52fe86ebf1134f2c7c6ba
Fanfarillo <matteo.fanfarillo99@gmail.com>
2023-02-10 16:56:31 +0100

    Debugging su Openvpn e NAT in corso...

--- scripts/hosts/lan-A/SetupCentralNode.sh
+++ scripts/hosts/lan-A/SetupCentralNode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip link set eth0 up
-ip addr add 10.23.1.2/24 dev eth0
+ip link set enp0s3 up
+ip addr add 10.23.1.2/24 dev enp0s3
 
 ip route add default via 10.23.1.1
--- scripts/hosts/lan-B/SetupOpenvpn-server.sh
+++ scripts/hosts/lan-B/SetupOpenvpn-server.sh
@@ -6,5 +6,4 @@ ip addr add 192.168.17.1/24 dev eth1
 
 ip route add default via 2.0.0.1
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 echo 1 > /proc/sys/net/ipv4/ip_forward
