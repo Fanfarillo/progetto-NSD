483892fc8582eb4fb2af89d2f35339b6d14c1a85
Sara-DaCanal <s.saradacanal@gmail.com>
2023-02-11 21:57:46 +0100

    fixata openvpn spero tanto

--- scripts/hosts/lan-B/NattingOpenvpn-server.sh
+++ scripts/hosts/lan-B/NattingOpenvpn-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 192.168.17.2
+iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o eth1 -j SNAT --to 192.168.16.2
+iptables -t nat -A PREROUTING -d 192.168.16.2 -i eth1 -j DNAT --to 192.168.100.6
