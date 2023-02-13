36c33412228969d57e4e16d591d5e3e1e8f6f7db
MegAtonBoom <baba.adrian13@gmail.com>
2023-02-13 18:11:40 +0100

    cea1 firewall modified- that should be all but i wouldn't be surprised if something will blow up

--- scripts/client-edges/FirewallCE-A1.sh
+++ scripts/client-edges/FirewallCE-A1.sh
@@ -4,19 +4,21 @@ export AS=enp0s3
 
 #flush configurazione precedente
 iptables -F
+iptables -F -t nat
+
 #drop all forward
 iptables -P FORWARD DROP
+
 #drop all input 
 iptables -P INPUT DROP
+
 #permettere tutte le uscite dal router
 iptables -P OUTPUT ACCEPT
 
 #permette tutto il traffico dalla lan in uscita
 iptables -A FORWARD -i $LAN -o $AS -j ACCEPT
 #con dynamic source address translation
-#iptables -t nat -A POSTROUTING -o $AS -j MASQUERADE
-iptables -t nat -A POSTROUTING -o $AS -j SNAT --to-source 10.23.0.10
-iptables -t nat -A POSTROUTING -o $AS -j SNAT --to-source 10.23.0.20
+iptables -t nat -A POSTROUTING -o $AS -j MASQUERADE
 
 #deny traffico verso il router tranne ssh e icmp dalla lan
 iptables -A INPUT -i $LAN -p tcp --dport 22 -j ACCEPT
@@ -24,9 +26,11 @@ iptables -A INPUT -i $LAN -p icmp -j ACCEPT
 
 #accetta entrata al router da connessioni stabilite
 iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+iptables -A FORWARD -m state ESTABLISHED -j ACCEPT
 
 #accetta l'ingresso di pacchetti http verso la lan
 iptables -A FORWARD -i $AS -o $LAN -p tcp --dport 80 -j ACCEPT
-#l'impostazione DNAT potrebbe non essere necessaria
-#iptables -t nat -A PREROUTING -i $AS -j DNAT --to-destination 10.23.0.10 
-#iptables -t nat -A PREROUTING -i $AS -j DNAT --to-destination 10.23.0.20
+
+#DNAT per connessione dall'esterno con HTTP
+iptables -t nat -A PREROUTING -i $AS -p tcp --dport 80 -j DNAT --to-destination 10.23.0.10 
+iptables -t nat -A PREROUTING -i $AS -p tcp --dport 8080 -j DNAT --to-destination 10.23.0.20
