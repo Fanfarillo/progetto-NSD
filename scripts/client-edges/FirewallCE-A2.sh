abf611256dc343e2c47c598fdc91cab56f2ca393
Sara-DaCanal <s.saradacanal@gmail.com>
2023-02-17 15:08:10 +0100

    sistemato il firewall di ce-a2

--- scripts/client-edges/FirewallCE-A2.sh
+++ scripts/client-edges/FirewallCE-A2.sh
@@ -9,14 +9,14 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 
 #permette comunicazione tra site2 e central node, e viceversa
-iptables -A FORWARD -s 10.23.1.2 -d 10.123.0.0/16 -j ACCEPT
-iptables -A FORWARD -s 10.123.0.0/16 -d 10.23.1.2 -j ACCEPT
+iptables -A FORWARD -s 10.23.1.0/24 -d 10.123.0.0/16 -j ACCEPT
+iptables -A FORWARD -s 10.123.0.0/16 -d 10.23.1.0/24 -j ACCEPT
+
+#permettere tutta la comunicazione icpm
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A FORWARD -p icmp -j ACCEPT
+iptables -A OUTPUT -p icmp -j ACCEPT
 
 #permette comunicazione tra gli spoke
 iptables -A FORWARD -s 10.23.1.0/24 -d 10.23.0.0/24 -j ACCEPT
 iptables -A FORWARD -s 10.23.0.0/24 -d 10.23.1.0/24 -j ACCEPT
-
-#natting
-iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 10.123.0.10
-iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 10.123.0.20
-iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 10.123.0.30
