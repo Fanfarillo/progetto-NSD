deeb7e59a0c5052ab7aba9fd671f6a33c93f530d
Sara-DaCanal <s.saradacanal@gmail.com>
2023-02-17 17:16:52 +0100

    corretti gravi problemi del sito

--- scripts/hosts/lan-A/Sito-web/SendToAV.sh
+++ scripts/hosts/lan-A/Sito-web/SendToAV.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-nc -q 10 10.123.0.10 50000 < binary &
-nc -q 10 10.123.0.20 50000< binary &
-nc -q 10 10.123.0.30 50000 < binary &
+echo "file non ancora pronto, aggiornare più tardi" > log_file/report
+rm -rf log1.log
+rm -rf log2.log
+rm -rf log3.log
+nc -lnvp 50001 > /var/www/html/log_file/log1.log &
+pid1=$!
+nc -lnvp 50002 > /var/www/html/log_file/log2.log &
+pid2=$!
+nc -lnvp 50003 > /var/www/html/log_file/log3.log &
+pid3=$!
 
-mkdir log_files
-cd log_files
+nc -q 10 10.123.0.10 50000 < uploads/binary &
+nc -q 10 10.123.0.20 50000 < uploads/binary &
+nc -q 10 10.123.0.30 50000 < uploads/binary &
 
-nc -lnvp 4000 > log1.log &
-nc -lnvp 4001 > log2.log &
-nc -lnvp 4002 > log3.log &
+wait $pid1
+wait $pid2
+wait $pid3
+
+echo "REPORT CLAMSCAN" > /var/www/html/log_file/report
+echo "" >> /var/www/html/log_file/report
+cat /var/www/html/log_file/log1.log >> /var/www/html/log_file/report
+echo "" >> /var/www/html/log_file/report
+echo "REPORT RKHUNTER" >> /var/www/html/log_file/report
+echo "" >> /var/www/html/log_file/report
+cat /var/www/html/log_file/log2.log >> /var/www/html/log_file/report
+echo "" >> /var/www/html/log_file/report
+echo "REPORT CHKROOTKIT" >> /var/www/html/log_file/report
+echo "Care: LKM Rootkit signeled by this antivirus is a well known possible false positive!" >> /var/www/html/log_file/report
+echo "" >> /var/www/html/log_file/report
+cat /var/www/html/log_file/log3.log >> /var/www/html/log_file/report
