87d93a3070c37c1518d2bd4331b54778ba9f855d
Sara-DaCanal <s.saradacanal@gmail.com>
2023-02-21 11:54:04 +0100

    errore corretto

--- scripts/client-edges/SetupCE-A1.sh
+++ scripts/client-edges/SetupCE-A1.sh
@@ -2,9 +2,7 @@
 
 sysctl -w net.ipv4.ip_forward=1
 ip link set enp0s3 up
-ip link set enp0s8 up
 ip addr add 100.1.12.2/30 dev enp0s3
-ip addr add 10.23.0.1/24 dev enp0s8
 
 ip route add default via 100.1.12.1
 
